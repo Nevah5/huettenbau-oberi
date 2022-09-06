564c340f961ccf97b8c443288da3c2144bf6af77
Nevah5 <neevaah5@gmail.com>
2022-09-06 23:49:51 +0200

    Exported routes into own file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  }
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from "@/router/routes"
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/router/routes.ts
@@ -0,0 +1,16 @@
+import {RouteRecordRaw} from "vue-router"
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () =>  () => import('@/views/AboutView.vue')
+  }
+]
+
+export default routes
