8369c8aaca6d0c2ecd97fd91d771402974c8465c
Nevah5 <neevaah5@gmail.com>
2022-09-09 12:18:12 +0200

    Added fontawesome to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faBars);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount('#app')
