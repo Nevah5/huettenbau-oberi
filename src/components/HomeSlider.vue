44bd66a25b2fef51d7bbbb85f0adbb7b8b5d2851
Nevah5 <neevaah5@gmail.com>
2022-09-12 20:31:51 +0200

    Moved slider into own component

---
+++ src/components/HomeSlider.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full p-24">
+    <figure class="relative">
+      <caption
+        class="absolute -translate-y-1/2 -translate-x-1/2 top-1/2 right-1/2"
+      >
+        ETsadf
+      </caption>
+      <img src="@/assets/images/slider.jpg" alt="Slider Image" class="w-full" />
+    </figure>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeSlider",
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="w-full p-24">
-    <figure class="relative">
-      <caption
-        class="absolute -translate-y-1/2 -translate-x-1/2 top-1/2 right-1/2"
-      >
-        ETsadf
-      </caption>
-      <img src="@/assets/images/slider.jpg" alt="Slider Image" class="w-full" />
-    </figure>
-  </div>
+  <HomeSlider />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HomeSlider from "@/components/HomeSlider.vue";
 
 export default defineComponent({
   name: "HomeView",
-  components: {},
+  components: { HomeSlider },
 });
 </script>
