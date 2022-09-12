7a17e86c07110ecc1195e460abda425c1e389654
Nevah5 <neevaah5@gmail.com>
2022-09-12 21:16:11 +0200

    Edited Styling of home slider

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="w-full p-24">
+  <div class="w-full m-auto md:my-6 md:max-w-[80%] w-full">
     <figure class="relative">
       <caption
-        class="absolute -translate-y-1/2 -translate-x-1/2 top-1/2 right-1/2"
+        class="absolute -translate-y-1/2 -translate-x-1/2 top-[70%] left-[80%] bg-white drop-shadow-md py-5 px-10 rounded-md w-[30vw]"
       >
-        ETsadf
+        <h1 class="text-3xl">Lust auf ein Abenteuer, ...</h1>
+        <p>... neue Freunde, Holz und Spass? Dann besuche uns jetzt.</p>
       </caption>
       <img src="@/assets/images/slider.jpg" alt="Slider Image" class="w-full" />
     </figure>
