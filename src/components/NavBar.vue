bd839d45dd3bdcb8d931ebdd9c3c0d81f8f5fc4e
Nevah5 <neevaah5@gmail.com>
2022-10-04 19:18:00 +0100

    Changed some html components

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav
+  <div
     class="sticky flex items-center justify-center flex-col bg-white top-0 w-full border-t-8 border-solid border-red shadow-navbar z-50"
   >
     <div class="flex items-center justify-between w-full h-fit px-4 my-4">
       <router-link to="/" class="logo w-64"></router-link>
-      <div class="hidden md:flex justify-end items-center text-black">
+      <nav class="hidden md:flex justify-end items-center text-black">
         <router-link
           to="/"
           class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
@@ -30,14 +30,14 @@
           class="py-2.5 px-3 mx-1 bg-red text-white font-bold rounded-md h-fit hover:bg-red-dark"
           >Login</router-link
         >
-      </div>
+      </nav>
       <font-awesome-icon
         icon="fas fa-bars"
         class="md:hidden h-6 p-3 hover:bg-black-light rounded-md cursor-pointer"
         @click="navbarState = !navbarState"
       />
     </div>
-    <nav
+    <!-- <nav
       v-if="navbarState"
       class="md:hidden bg-white border-red border-solid border-y-2 p-4 w-full"
     >
@@ -66,8 +66,8 @@
         class="py-2.5 px-3 mx-1 bg-red text-white font-bold rounded-md h-fit hover:bg-red-dark"
         >Login</router-link
       >
-    </nav>
-  </nav>
+    </nav> -->
+  </div>
 </template>
 
 <script lang="ts">
