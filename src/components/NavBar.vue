a90bab515ea5f31d62778e769e0fcf203b64414e
Nevah5 <neevaah5@gmail.com>
2022-10-09 17:04:16 +0200

    Added infos navigation links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,12 +5,17 @@
     <!-- Main Navbar -->
     <div class="flex items-center justify-between w-full">
       <router-link to="/" class="logo w-64"></router-link>
-      <nav class="hidden md:flex justify-end items-center text-black">
+      <nav class="hidden lg:flex justify-end items-center text-black">
         <router-link
           @click="resetStates()"
           to="/"
           class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
           >Startseite</router-link
+        >
+        <a
+          @click="resetStates()"
+          class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light cursor-pointer"
+          >Infos</a
         >
         <a
           @click="galleryState = !galleryState"
@@ -38,13 +43,13 @@
       </nav>
       <font-awesome-icon
         icon="fas fa-bars"
-        class="md:hidden p-3 hover:bg-black-light rounded-md cursor-pointer h-[20px]"
+        class="lg:hidden p-3 hover:bg-black-light rounded-md cursor-pointer h-[20px]"
         @click="toggleNavbar()"
       />
     </div>
     <!-- Gallery (PC) -->
     <div
-      class="hidden md:flex w-full justify-end text-black flex-wrap"
+      class="hidden lg:flex w-full justify-end text-black flex-wrap"
       v-auto-animate
     >
       <router-link
@@ -147,7 +152,7 @@
       >
     </div>
     <!-- Navbar Navigation (Phone) -->
-    <div class="md:hidden w-full" v-auto-animate>
+    <div class="lg:hidden w-full" v-auto-animate>
       <div v-if="navbarState" class="w-full h-[2px] my-4 bg-red"></div>
       <nav v-if="navbarState" class="flex justify-end flex-col">
         <router-link
@@ -156,6 +161,66 @@
           class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light my-1"
           >Startseite</router-link
         >
+        <a
+          @click="infoState = !infoState"
+          class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light cursor-pointer"
+          >Infos</a
+        >
+        <!-- Infos -->
+        <div
+          class="flex w-7/8 justify-start text-black flex-wrap pl-4 border-red border-l-2 ml-4"
+          v-auto-animate
+        >
+          <router-link
+            v-if="infoState"
+            to="/infos/dachverband"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Dachverband</router-link
+          >
+          <router-link
+            v-if="infoState"
+            to="/infos/lager"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Lager</router-link
+          >
+          <router-link
+            v-if="infoState"
+            to="/infos/team"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Team</router-link
+          >
+          <router-link
+            v-if="infoState"
+            to="/infos/verein"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Verein</router-link
+          >
+          <router-link
+            v-if="infoState"
+            to="/infos/standort"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Standort</router-link
+          >
+          <router-link
+            v-if="infoState"
+            to="/infos/sponsoren"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Sponsoren</router-link
+          >
+          <router-link
+            v-if="infoState"
+            to="/infos/links"
+            @click="resetStates()"
+            class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+            >Links</router-link
+          >
+        </div>
         <a
           @click="galleryState = !galleryState"
           class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light cursor-pointer my-1"
@@ -274,7 +339,7 @@
         <router-link
           @click="resetStates()"
           to="/mehr"
-          class="py-2.5 px-3 font-bold rounded-md h-fit hover:bg-black-light my-1"
+          class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light my-1"
           >Mehr</router-link
         >
         <router-link
@@ -293,6 +358,7 @@ import { defineComponent } from "vue";
 
 interface Data {
   navbarState: boolean;
+  infoState: boolean;
   galleryState: boolean;
 }
 
@@ -301,6 +367,7 @@ export default defineComponent({
   data(): Data {
     return {
       navbarState: false,
+      infoState: false,
       galleryState: false,
     };
   },
