1130ddb6c924619cf03594575ca6ac6390b43341
Nevah5 <neevaah5@gmail.com>
2022-10-10 23:23:20 +0200

    #27 Added navbardata interface

--- src/vue/navbar.ts
+++ src/vue/navbar.ts
@@ -1,13 +1,29 @@
 import { firestore } from "@/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
+interface navbarData {
+  id: string,
+  display: string,
+  link?: string,
+  isExternal?: boolean,
+  order: number,
+  sub?: {
+    display: string,
+    link: string,
+    isExternal: boolean,
+    order: number
+  }[]
+}
+
 const getNavbarData = async () => {
   const res = await getDocs(collection(firestore, "navbar"))
   const data = res.docs.map(doc => {
     return { id: doc.id, ...doc.data() }
   })
+  return data;
 }
 
 export {
+  navbarData,
   getNavbarData
 }
