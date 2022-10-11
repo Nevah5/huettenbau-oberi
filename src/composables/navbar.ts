1df045aab2427ee06e12271fda9fbd1a06611898
Nevah5 <neevaah5@gmail.com>
2022-10-11 22:29:32 +0200

    Fixed #36

--- src/composables/navbar.ts
+++ src/composables/navbar.ts
@@ -18,8 +18,13 @@ interface navbarItem {
 const getNavbarData = async () => {
   const res = await getDocs(query(collection(firestore, "navbar"), orderBy("order", "asc")))
   const data = res.docs.map(doc => {
-    return { id: doc.id, ...doc.data() }
+    return { id: doc.id, ...doc.data() } as navbarItem
   })
+  data.forEach(d => {
+    d.sub?.sort((a, b) => {
+      return a.order - b.order
+    })
+  });
   return (data as navbarItem[]);
 }
 
