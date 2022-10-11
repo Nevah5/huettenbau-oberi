6e1508783942b4af93d4f35ad82fae6f67f37717
Nevah5 <neevaah5@gmail.com>
2022-10-11 09:27:58 +0200

    #27 Added login to display all the links

--- src/components/NavbarLink.vue
+++ src/components/NavbarLink.vue
@@ -1,19 +1,34 @@
 <template>
+  <router-link
+    v-if="isSiteLinkWithoutSubs(data)"
+    :to="data.link!"
+    class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light"
+    >{{ data.display }}</router-link
+  >
   <a
+    v-else
+    :href="data.link!"
     class="py-2.5 px-3 mx-1 font-bold rounded-md h-fit hover:bg-black-light cursor-pointer"
-    >{{ d.display }}</a
+    >{{ data.display }}</a
   >
 </template>
 
 <script lang="ts">
+import { defineEmits } from "vue";
 import { navbarItem } from "@/vue/navbar";
 
 export default {
   name: "NavbarLink",
   props: ["doc"],
   setup(props: any) {
+    const isSiteLinkWithoutSubs = (data: navbarItem): boolean => {
+      if (!data.sub && !data.isExternal) return true;
+      return false;
+    };
+
     return {
-      d: props.doc as navbarItem,
+      data: props.doc as navbarItem,
+      isSiteLinkWithoutSubs,
     };
   },
 };
