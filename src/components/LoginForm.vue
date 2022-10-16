6e710c34c69f9540b0c8a3d39c2cfbcc906bdfe8
Nevah5 <neevaah5@gmail.com>
2022-10-16 20:51:19 +0200

    #43 Finished Login Form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,20 +2,25 @@
   <section class="flex justify-center items-center h-screen">
     <form
       @submit.prevent="submit"
-      class="w-5/12 border-red border-dashed border-2 flex flex-col justify-center items-center p-4"
+      class="w-5/12 flex flex-col justify-center items-center p-16 text-black rounded drop-shadow-xl shadow-2xl"
     >
       <router-link to="/" class="logo w-3/4 mb-4"></router-link>
-      <h2 class="font-bold text-black text-xl mb-4">Logge dich hier ein:</h2>
+      <h2 class="font-bold text-xl mb-4">Logge dich hier ein:</h2>
       <input
         type="text"
         placeholder="Email"
-        class="border-red border-solid border-2 rounded w-full"
+        class="appearance-none block border-red border-solid border-2 rounded w-full px-4 py-2 mb-2"
+      />
+      <input
+        type="password"
+        placeholder="Passwort"
+        class="appearance-none block border-red border-solid border-2 rounded w-full px-4 py-2 mb-2"
       />
       <button
         type="submit"
-        class="w-full bg-red rounded text-white font-bold p-2 mt-4"
+        class="w-full bg-red rounded text-white font-bold p-2 mt-2"
       >
-        Submit
+        Einloggen
       </button>
     </form>
   </section>
