1e808a056243d1673899c67b8ae74b96b5c89168
Nevah5 <neevaah5@gmail.com>
2022-10-18 23:13:43 +0200

    #58 Added logout button into account overview view

--- src/views/account/OverviewView.vue
+++ src/views/account/OverviewView.vue
@@ -1,3 +1,25 @@
 <template>
-  <router-link to="/account/logout"> Logout </router-link>
+  <button
+    class="text-white font-bold bg-red rounded-md py-2 px-4"
+    @click="logout"
+  >
+    Logout
+  </button>
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { logoutUser } from "@/composables/account";
+
+const router = useRouter();
+
+const logout = () => {
+  logoutUser()
+    .then(() => {
+      router.push("/");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+</script>
