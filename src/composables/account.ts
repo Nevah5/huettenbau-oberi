4c2ddcdfda8f899879528acfbba11e21aa892b37
Nevah5 <neevaah5@gmail.com>
2022-10-18 22:05:33 +0200

    #58 Added loginWithEmailAndPassword function

--- src/composables/account.ts
+++ src/composables/account.ts
@@ -1,15 +1,35 @@
-import { getAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { app } from "../firebase"
 
-const loginWithEmailAndPassword = (email: string, password: string) => {
-  return new Promise<UserCredential>((resolve, reject) => {
-    try {
-      const auth = getAuth(app)
-      const user = signInWithEmailAndPassword(auth, email, password)
-      resolve(user)
-    }catch(e){
-      reject(e)
-    }
+const loginWithEmailAndPassword = (email: string, password: string): Promise<string | void> => {
+  return new Promise<string | void>((resolve, reject) => {
+    const auth = getAuth(app)
+    signInWithEmailAndPassword(auth, email, password)
+      .then((data) => {
+        console.log(data);
+        resolve()
+      })
+      .catch((e) => {
+        let errorMessage = "";
+        switch(e.code){
+          case "auth/invalid-email":
+            errorMessage = "Ungültige Email Adresse"
+            break;
+          case "auth/user-not-found":
+            errorMessage = "Benutzer nicht gefunden"
+            break;
+          case "auth/wrong-password":
+            errorMessage = "Passwort inkorrekt"
+            break;
+          case "auth/user-disabled":
+            errorMessage = "Dieser Benutzer wurde ausgeschaltet"
+            break;
+          default:
+            errorMessage = "Etwas ist schief gelaufen!"
+            console.log(e);
+        }
+        reject(errorMessage);
+      })
   })
 }
 
