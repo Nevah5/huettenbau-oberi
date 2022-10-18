8ece036b43cb4479d19b072d69ec5adc166796c0
Nevah5 <neevaah5@gmail.com>
2022-10-18 23:03:37 +0200

    #58 Updated routes with account view

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -18,6 +18,36 @@ const routes: RouteRecordRaw[] = [
       enforceNoLogin: true,
     },
     component: () => import('@/views/LoginView.vue')
+  },
+  {
+    path: '/account',
+    redirect: '/account/overview',
+    name: 'account',
+    meta: {
+      titleName: 'Account',
+      enforceLogin: true,
+    },
+    component: () => import('@/views/AccountView.vue'),
+    children: [
+      {
+        path: 'overview',
+        name: 'overview',
+        meta: {
+          titleName: 'Account',
+          enforceLogin: true,
+        },
+        component: () => import('@/views/account/OverviewView.vue')
+      },
+      {
+        path: 'logout',
+        name: 'logout',
+        meta: {
+          titleName: 'Logging out...',
+          enforceLogin: true,
+        },
+        component: () => import('@/views/account/LogoutView.vue')
+      }
+    ]
   }
 ]
 
