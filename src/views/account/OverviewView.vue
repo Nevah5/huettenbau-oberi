18b8389f3aec5562d027cb1ceba4d6eb97058642
Nevah5 <neevaah5@gmail.com>
2022-10-19 17:06:57 +0200

    #63 Added edit link to displayname and email in overview

--- src/views/account/OverviewView.vue
+++ src/views/account/OverviewView.vue
@@ -1,11 +1,21 @@
 <template>
   <section class="w-full px-4 sm:w-9/12 sm:px-0 xl:w-1/2">
     <div class="mb-8 text-black">
-      <h2 class="text-xl font-bold mb-2">Profil</h2>
+      <!-- Title -->
+      <h2 class="text-xl font-bold mb-2">
+        Profil{{ isGoogleAccount() ? " [" : ""
+        }}<font-awesome-icon
+          icon="fa-brands fa-google"
+          v-if="isGoogleAccount()"
+        />{{ isGoogleAccount() ? " Google]" : "" }}
+      </h2>
+      <!-- Line -->
       <div class="w-full bg-black h-[2px] block mb-2"></div>
+      <!-- Content -->
       <div
         class="flex justify-start sm:flex-row mt-4 flex-wrap w-full flex-col"
       >
+        <!-- Avatar -->
         <div
           class="flex flex-col justify-start items-center w-fit sm:w-1/3 mb-4 sm:mb-0"
         >
@@ -15,9 +25,14 @@
           />
           <span class="w-fit font-bold">Avatar</span>
         </div>
+        <!-- Data -->
         <div class="flex flex-col justify-start items-start sm:w-2/3 w-full">
+          <!-- Display Name -->
           <div class="mb-4 w-full">
-            <label for="displayName" class="font-bold">Anzeige Name</label>
+            <div class="flex justify-between">
+              <label for="displayName" class="font-bold">Anzeige Name</label>
+              <a href="#edit=displayName" class="text-red underline">Ändern</a>
+            </div>
             <div class="flex justify-center items-center">
               <input
                 type="text"
@@ -28,8 +43,17 @@
               />
             </div>
           </div>
+          <!-- Email -->
           <div class="mb-4 w-full">
-            <label for="email" class="font-bold">Email Adresse</label>
+            <div class="flex justify-between">
+              <label for="email" class="font-bold">Email Adresse</label>
+              <a
+                href="#edit=email"
+                class="text-red underline"
+                v-if="!isGoogleAccount()"
+                >Ändern</a
+              >
+            </div>
             <div class="flex justify-center items-center">
               <input
                 type="text"
@@ -40,6 +64,7 @@
               />
             </div>
           </div>
+          <!-- User ID -->
           <div class="mb-4 w-full">
             <label for="uid" class="font-bold">User ID</label>
             <div class="flex justify-center items-center">
@@ -85,7 +110,11 @@
 
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
-import { logoutUser, loggedInUser } from "@/composables/account";
+import {
+  logoutUser,
+  loggedInUser,
+  isGoogleAccount,
+} from "@/composables/account";
 
 const user = loggedInUser();
 const router = useRouter();
