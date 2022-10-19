cab52ae953394b9e05606a3abb67ab43b052a515
Nevah5 <neevaah5@gmail.com>
2022-10-19 12:45:37 +0200

    #63 Added temp delete and deactivate button into account Overview

--- src/views/account/OverviewView.vue
+++ src/views/account/OverviewView.vue
@@ -1,14 +1,28 @@
 <template>
   <section class="w-full px-4 sm:w-9/12 sm:px-0 xl:w-1/2">
     <div class="mb-8">
-      <h2 class="text-xl font-bold text-black mb-2">Aktionen</h2>
+      <h2 class="text-xl font-bold text-black mb-2">Account</h2>
       <div class="w-full bg-black h-[2px] block mb-2"></div>
-      <p class="text-black mb-2">Möchtest du dich ausloggen? Kein Problem.</p>
+      <p class="text-black mb-2">
+        Hier kannst du verschidene Aktionen mit deinem Account durchführen. Du
+        solltest aber mit <b>VORSICHT</b> vorgehen, da diese meist nicht
+        wiederherstellbar sind.
+      </p>
       <button
-        class="text-white font-bold bg-red rounded-md py-2 px-4"
+        class="text-white font-bold bg-red rounded-md border-2 border-red py-2 px-4 mr-2"
         @click="logout"
       >
         Logout
+      </button>
+      <button
+        class="text-red font-bold bg-white border-red border-2 rounded-md py-2 px-4 mr-2 hover:bg-red hover:text-white"
+      >
+        DEAKTIVIEREN
+      </button>
+      <button
+        class="text-red font-bold bg-white border-red border-2 rounded-md py-2 px-4 mr-2 hover:bg-red hover:text-white"
+      >
+        LÖSCHEN
       </button>
     </div>
   </section>
