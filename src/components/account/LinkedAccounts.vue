c42231d2b0020583dfad263f91b289cea4e2f455
Nevah5 <neevaah5@gmail.com>
2022-10-19 22:35:40 +0200

    #78 Added LinkedAccounts component

---
+++ src/components/account/LinkedAccounts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mb-8 text-black">
+    <h2 class="text-xl font-bold mb-2">Verknüpfte Accounts</h2>
+    <div class="w-full bg-black h-[2px] block mb-2"></div>
+    <!-- Google -->
+    <div class="p-8 flex justify-start items-center gap-4">
+      <font-awesome-icon icon="fa-brands fa-google" class="h-[50px]" />
+      <button
+        class="text-black border-black border-2 border-solid rounded-md px-4 py-2 font-bold hover:bg-black hover:text-white"
+      >
+        Verlinken
+      </button>
+    </div>
+    <div class="p-8 flex justify-start items-center gap-4">
+      <font-awesome-icon icon="fa-brands fa-google" class="h-[50px]" />
+      <p>neevaah5@gmail.com</p>
+      <button
+        class="text-black border-black border-2 border-solid rounded-md px-4 py-2 font-bold hover:bg-black hover:text-white"
+      >
+        Aufheben
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
