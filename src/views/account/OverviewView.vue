150793241d71eca4a893dd6cabb80bd115a7f759
Nevah5 <neevaah5@gmail.com>
2022-10-19 11:49:48 +0200

    #63 Styled Logout Section

--- src/views/account/OverviewView.vue
+++ src/views/account/OverviewView.vue
@@ -1,10 +1,15 @@
 <template>
-  <button
-    class="text-white font-bold bg-red rounded-md py-2 px-4"
-    @click="logout"
-  >
-    Logout
-  </button>
+  <section class="w-full px-4 sm:w-9/12 sm:px-0 xl:w-1/2">
+    <h2 class="text-xl font-bold text-black mb-2">Aktionen</h2>
+    <div class="w-full bg-black h-[2px] block mb-2"></div>
+    <p class="text-black mb-2">Möchtest du dich ausloggen? Kein Problem.</p>
+    <button
+      class="text-white font-bold bg-red rounded-md py-2 px-4"
+      @click="logout"
+    >
+      Logout
+    </button>
+  </section>
 </template>
 
 <script lang="ts" setup>
