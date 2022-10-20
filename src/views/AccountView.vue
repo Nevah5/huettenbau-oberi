807f36523a0b6df82deea606e89bfd356ff45a41
Nevah5 <neevaah5@gmail.com>
2022-10-20 17:23:09 +0200

    #69 Added opacity transition to account settings

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,10 +1,26 @@
 <template>
   <main class="w-full flex flex-col justify-start items-center">
     <AccountNav />
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="slide">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </main>
 </template>
 
 <script lang="ts" setup>
 import AccountNav from "@/components/account/AccountNav.vue";
 </script>
+
+<style lang="scss" scoped>
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 500ms ease-in-out;
+}
+</style>
