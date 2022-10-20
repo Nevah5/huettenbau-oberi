1eee6221fcf782c74b8d80b1d098437b0722b16f
Nevah5 <neevaah5@gmail.com>
2022-10-20 21:06:16 +0200

    #84 Added admin view

---
+++ src/views/AdminView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="w-full flex flex-col justify-start items-center">
+    <AdminNav />
+    <router-view v-slot="{ Component }">
+      <transition name="settings" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import AdminNav from "@/components/admin/AdminNav.vue";
+</script>
+
+<style lang="scss" scoped>
+.settings-enter-from,
+.settings-leave-to {
+  opacity: 0;
+}
+
+.settings-enter-active,
+.settings-leave-active {
+  transition: opacity 300ms ease-in-out;
+}
+</style>
