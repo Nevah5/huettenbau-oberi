14ab447b6805687b4101c4daf882ce626bc58fbc
Nevah5 <neevaah5@gmail.com>
2022-10-20 17:42:26 +0200

    #69 Modified Account settings transitions

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -2,7 +2,7 @@
   <main class="w-full flex flex-col justify-start items-center">
     <AccountNav />
     <router-view v-slot="{ Component }">
-      <transition name="slide" mode="out-in">
+      <transition name="settings" mode="out-in">
         <component :is="Component" />
       </transition>
     </router-view>
@@ -14,13 +14,13 @@ import AccountNav from "@/components/account/AccountNav.vue";
 </script>
 
 <style lang="scss" scoped>
-.slide-enter-from,
-.slide-leave-to {
+.settings-enter-from,
+.settings-leave-to {
   opacity: 0;
 }
 
-.slide-enter-active,
-.slide-leave-active {
-  transition: opacity 500ms ease-in-out;
+.settings-enter-active,
+.settings-leave-active {
+  transition: opacity 300ms ease-in-out;
 }
 </style>
