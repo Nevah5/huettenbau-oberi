44d078d2e1700eac98783409b9d22e10ab8e13d7
Nevah5 <neevaah5@gmail.com>
2022-10-21 12:14:55 +0200

    #84 Display firestore navbar data in navigation  managment

--- src/views/admin/SiteNavigation.vue
+++ src/views/admin/SiteNavigation.vue
@@ -15,34 +15,43 @@
         <p class="text-sm w-[200px]">Anzeigetext</p>
         <p class="text-sm w-[400px]">Link</p>
       </div>
-      <div class="mb-2">
+      <div class="mb-2" v-for="item in (data as navbarItem[])" :key="item.id">
         <div class="flex flex-row gap-5 justify-start flex-nowrap">
           <input
             type="text"
+            :value="item.order"
             class="appearance-none w-[50px] h-[50px] border-black border-2 text-center rounded-md font-bold"
           />
           <input
             type="text"
+            :value="item.display"
             class="appearance-none w-[200px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
           />
           <input
             type="text"
+            v-if="!item.sub"
+            :value="item.link"
             class="appearance-none w-[400px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
           />
         </div>
         <div
           class="border-l-black border-l-2 p-2 ml-2 mt-2 flex flex-row justify-start flex-nowrap gap-3"
+          v-for="sub in item.sub"
+          :key="sub.display"
         >
           <input
             type="text"
+            :value="sub.order"
             class="appearance-none w-[50px] h-[50px] border-black border-2 text-center rounded-md font-bold"
           />
           <input
             type="text"
+            :value="sub.display"
             class="appearance-none w-[200px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
           />
           <input
             type="text"
+            :value="sub.link"
             class="appearance-none w-[330px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
           />
           <button class="rounded-md bg-red font-bold text-white px-4 py-2">
@@ -52,7 +61,10 @@
         <div
           class="border-l-black border-l-2 p-2 m-2 flex flex-row justify-start flex-nowrap gap-3"
         >
-          <button class="rounded-md bg-red font-bold text-white px-4 py-2">
+          <button
+            class="rounded-md bg-red font-bold text-white px-4 py-2"
+            v-if="item.sub"
+          >
             <font-awesome-icon icon="fa-solid fa-plus" />
             Link hinzufügen
           </button>
@@ -66,3 +78,14 @@
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { getNavbarData, navbarItem } from "@/composables/navbar";
+import { onMounted, ref } from "vue";
+
+const data = ref();
+
+onMounted(async () => {
+  data.value = await getNavbarData();
+});
+</script>
