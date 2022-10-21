58a90e6e0bce05a8982e247e63616fbf7e135abd
Nevah5 <neevaah5@gmail.com>
2022-10-21 11:29:45 +0200

    #84 Added form structure for navigation managment

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import {autoAnimatePlugin} from "@formkit/auto-animate/vue"
 import './assets/tailwind.css'
 
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faBars, faArrowUpRightFromSquare, faCircleNotch, faUnlock } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faArrowUpRightFromSquare, faCircleNotch, faUnlock, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons"
 import {faGoogle} from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
-library.add(faBars, faArrowUpRightFromSquare, faCircleNotch, faGoogle, faUnlock)
+library.add(faBars, faArrowUpRightFromSquare, faCircleNotch, faGoogle, faUnlock, faPlus, faTrash)
 
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
--- src/views/admin/SiteNavigation.vue
+++ src/views/admin/SiteNavigation.vue
@@ -4,7 +4,65 @@
       Verwaltung Navigation
     </h2>
     <p class="mb-4">
-      Auf dieser Seite kannst du die Navigationsleiste oben verwalten.
+      Auf dieser Seite kannst du die Navigationsleiste oben verwalten.<br />
+      Zurzeit kann keine Hauptlinks hinzufügen oder entfernen, da das Responsive
+      Design fix in die Seite integriert wurde, daher melde dich bei Änderungen
+      gleich bei Noah.
     </p>
+    <div>
+      <div class="flex flex-row gap-5 justify-start flex-nowrap">
+        <p class="text-sm w-[50px]">Nr.</p>
+        <p class="text-sm w-[200px]">Anzeigetext</p>
+        <p class="text-sm w-[400px]">Link</p>
+      </div>
+      <div class="mb-2">
+        <div class="flex flex-row gap-5 justify-start flex-nowrap">
+          <input
+            type="text"
+            class="appearance-none w-[50px] h-[50px] border-black border-2 text-center rounded-md font-bold"
+          />
+          <input
+            type="text"
+            class="appearance-none w-[200px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
+          />
+          <input
+            type="text"
+            class="appearance-none w-[400px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
+          />
+        </div>
+        <div
+          class="border-l-black border-l-2 p-2 ml-2 mt-2 flex flex-row justify-start flex-nowrap gap-3"
+        >
+          <input
+            type="text"
+            class="appearance-none w-[50px] h-[50px] border-black border-2 text-center rounded-md font-bold"
+          />
+          <input
+            type="text"
+            class="appearance-none w-[200px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
+          />
+          <input
+            type="text"
+            class="appearance-none w-[330px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
+          />
+          <button class="rounded-md bg-red font-bold text-white px-4 py-2">
+            <font-awesome-icon icon="fa-solid fa-trash" />
+          </button>
+        </div>
+        <div
+          class="border-l-black border-l-2 p-2 m-2 flex flex-row justify-start flex-nowrap gap-3"
+        >
+          <button class="rounded-md bg-red font-bold text-white px-4 py-2">
+            <font-awesome-icon icon="fa-solid fa-plus" />
+            Link hinzufügen
+          </button>
+        </div>
+      </div>
+      <div class="flex flex-row justify-start gap-3 mt-8">
+        <button class="rounded-md bg-red font-bold text-white px-4 py-2">
+          Speichern
+        </button>
+      </div>
+    </div>
   </div>
 </template>
