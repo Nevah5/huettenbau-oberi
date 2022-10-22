21143c90c03894ea34c5226f0d2a9a18cde9c346
Nevah5 <neevaah5@gmail.com>
2022-10-22 13:06:44 +0200

    #84 Display admins in permission managment

---
+++ src/views/admin/SitePermissions.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold border-b-2 border-black border-b-solid mb-2">
+      Verwaltung Zugriff
+    </h2>
+    <p class="mb-4">
+      Auf dieser Seite kannst du die Administrationsbenutzer verwalten, die auf
+      den Admin bereich Zugriff haben.
+    </p>
+    <h3>Benutzer:</h3>
+    <p v-for="admin in admins" :key="admin">{{ admin }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getAdminUsers } from "@/composables/config";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const admins = ref();
+
+onMounted(async () => {
+  let data = await getAdminUsers();
+  if (data) {
+    let adminUsers: string[] = [];
+    data.users?.forEach((user: string) => {
+      adminUsers.push(user);
+    });
+    admins.value = adminUsers;
+  }
+});
+</script>
