7af8e1d73f7e5a4b669ceb79a4793db146f38f09
Nevah5 <neevaah5@gmail.com>
2022-10-22 12:55:50 +0200

    #84 Added restriction to admin panel

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="w-full flex flex-row justify-start items-start">
+  <main class="w-full flex flex-row justify-start items-start" v-if="admins">
     <AdminNav />
     <div class="m-8 text-black w-full">
       <router-view v-slot="{ Component }">
@@ -13,6 +13,29 @@
 
 <script lang="ts" setup>
 import AdminNav from "@/components/admin/AdminNav.vue";
+import { getAdminUsers } from "@/composables/config";
+import { loggedInUser } from "@/composables/account";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const admins = ref();
+
+onMounted(async () => {
+  let data = await getAdminUsers();
+  if (data) {
+    let adminUsers: string[] = [];
+    data.users?.forEach((user: string) => {
+      adminUsers.push(user);
+    });
+    admins.value = adminUsers;
+  }
+  let user = loggedInUser();
+  if (user) {
+    if (!(admins.value as string[]).includes(user.uid))
+      return router.push("/nopermissions");
+  }
+});
 </script>
 
 <style lang="scss" scoped>
