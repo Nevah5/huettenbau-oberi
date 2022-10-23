40b888a4c375f573e4e6e54f2ebe57ee0b15be5c
Nevah5 <neevaah5@gmail.com>
2022-10-23 22:31:31 +0200

    #84 Changed styling of nav manamgent

--- src/components/admin/NavigationManagment.vue
+++ src/components/admin/NavigationManagment.vue
@@ -7,13 +7,13 @@
     Daten werden geladen...
   </p>
   <div v-else>
-    <div class="flex flex-row gap-5 justify-start flex-nowrap">
+    <div class="flex flex-row gap-3 justify-start flex-nowrap">
       <p class="text-sm w-[50px]">Nr.</p>
       <p class="text-sm w-[200px]">Anzeigetext</p>
       <p class="text-sm w-[400px]">Link</p>
     </div>
     <div class="mb-2" v-for="item in (data as navbarItem[])" :key="item.id">
-      <div class="flex flex-row gap-5 justify-start flex-nowrap">
+      <div class="flex flex-row gap-3 justify-start flex-nowrap">
         <input
           type="text"
           :value="item.order"
@@ -30,47 +30,50 @@
           :value="item.link"
           class="appearance-none w-[400px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
         />
-      </div>
-      <div
-        class="border-l-black border-l-2 p-2 ml-2 mt-2 flex flex-row justify-start flex-nowrap gap-3"
-        v-for="sub in item.sub"
-        :key="sub.display"
-      >
-        <input
-          type="text"
-          :value="sub.order"
-          class="appearance-none w-[50px] h-[50px] border-black border-2 text-center rounded-md font-bold"
-        />
-        <input
-          type="text"
-          :value="sub.display"
-          class="appearance-none w-[200px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
-        />
-        <input
-          type="text"
-          :value="sub.link"
-          class="appearance-none w-[330px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
-        />
-        <button class="rounded-md bg-red font-bold text-white px-4 py-2">
-          <font-awesome-icon icon="fa-solid fa-trash" />
-        </button>
-      </div>
-      <div
-        class="border-l-black border-l-2 p-2 m-2 flex flex-row justify-start flex-nowrap gap-3"
-      >
         <button
-          class="rounded-md bg-red font-bold text-white px-4 py-2"
-          v-if="item.sub"
+          class="flex justify-start items-center gap-2 rounded-md bg-red font-bold text-white px-4 py-2"
         >
-          <font-awesome-icon icon="fa-solid fa-plus" />
-          Link hinzufügen
+          <font-awesome-icon icon="fa-solid fa-floppy-disk" />
+          <p>Speichern</p>
         </button>
       </div>
-    </div>
-    <div class="flex flex-row justify-start gap-3 mt-8">
-      <button class="rounded-md bg-red font-bold text-white px-4 py-2">
-        Speichern
-      </button>
+      <div v-if="item.sub">
+        <div
+          class="border-l-black border-l-2 p-2 pl-4 ml-2 mt-2 flex flex-row justify-start flex-nowrap gap-3"
+          v-for="sub in item.sub"
+          :key="sub.display"
+        >
+          <input
+            type="text"
+            :value="sub.order"
+            class="appearance-none w-[50px] h-[50px] border-black border-2 text-center rounded-md font-bold"
+          />
+          <input
+            type="text"
+            :value="sub.display"
+            class="appearance-none w-[200px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
+          />
+          <input
+            type="text"
+            :value="sub.link"
+            class="appearance-none w-[330px] h-[50px] border-black border-2 p-2 rounded-md font-bold"
+          />
+          <button class="rounded-md bg-red font-bold text-white px-4 py-2">
+            <font-awesome-icon icon="fa-solid fa-trash" />
+          </button>
+        </div>
+        <div
+          class="border-l-black border-l-2 p-2 m-2 flex flex-row justify-start flex-nowrap gap-3"
+        >
+          <button
+            class="rounded-md bg-red font-bold text-white px-4 py-2"
+            v-if="item.sub"
+          >
+            <font-awesome-icon icon="fa-solid fa-plus" />
+            Link hinzufügen
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
