c2b0a2423bd35ae102609b0cf0340b0a9fc8f690
Nevah5 <neevaah5@gmail.com>
2022-10-24 21:01:40 +0200

    #84 Added composable for external links

---
+++ src/composables/externalLinks.ts
@@ -0,0 +1,30 @@
+import { firestore } from "@/firebase"
+import { collection, doc, DocumentData, getDocs, query, QueryDocumentSnapshot, setDoc } from "firebase/firestore"
+
+interface link {
+  id: string,
+  link: string,
+  code?: string
+}
+
+const getExternalLinks = (): Promise<void | link[]> => {
+  return new Promise<void | link[]>((resolve, reject) => {
+    getDocs(query(collection(firestore, "external-links")))
+    .then((data) => {
+      const links: link[] = [];
+      data.docs.forEach((docSnapshot: QueryDocumentSnapshot<DocumentData>) => {
+        links.push(docSnapshot.data() as link)
+      })
+      resolve(links)
+    })
+    .catch((e) => {
+      console.log(e);
+      reject()
+    })
+  })
+}
+
+export {
+  link,
+  getExternalLinks
+}
