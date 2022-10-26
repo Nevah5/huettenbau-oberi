b056e145e0835c0a82c7702b6bfed755324c590c
Nevah5 <neevaah5@gmail.com>
2022-10-26 23:12:00 +0200

    #101 Added title with short description

---
+++ src/components/admin/gallery/GalleryManagment.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <h2
+        class="text-2xl font-bold border-b-2 border-black border-b-solid mb-2"
+      >
+        Verwaltung Galerien
+      </h2>
+      <p class="mb-4">Hier kannst du Galerien verwalten.</p>
+    </div>
+  </div>
+</template>
---
+++ src/views/admin/GalleryAdministration.vue
@@ -0,0 +1,7 @@
+<template>
+  <GalleryManagment />
+</template>
+
+<script setup lang="ts">
+import GalleryManagment from "@/components/admin/gallery/GalleryManagment.vue";
+</script>
