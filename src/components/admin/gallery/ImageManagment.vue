aeb63c89089bf471a537c92b8c39eba6c79d4efc
Nevah5 <neevaah5@gmail.com>
2022-10-27 16:21:43 +0200

    #101 Added loading animation to gallery image managment

--- src/components/admin/gallery/ImageManagment.vue
+++ src/components/admin/gallery/ImageManagment.vue
@@ -5,21 +5,76 @@
     </h2>
     <p class="mb-4">
       Dieser Abschnitt ist für das Verwalten der Bilder und Beschreibungen
-      dieser.
+      dieser.<br />Klicke auf die Gallery die du verwalten möchtest:
     </p>
-    <div v-if="!data" class="flex justify-start items-center">
+    <div v-if="!galleryData" class="flex justify-start items-center">
       <font-awesome-icon
         icon="fa-solid fa-circle-notch"
         class="animate-spin mr-1"
       />
       <p>Daten werden geladen</p>
     </div>
-    <div v-else>Bilder werden hier Verwaltet...</div>
+    <div v-else>
+      <div class="flex justify-start items-center gap-2">
+        <p
+          v-for="(gallery, i) in galleryData"
+          :key="gallery.id"
+          class="rounded-md p-2 px-3 font-bold hover:bg-black-light relative w-fit cursor-pointer"
+          :id="selectedGallery === i ? 'selected' : ''"
+          @click="selectedGallery = i"
+        >
+          {{ gallery.id }}
+        </p>
+      </div>
+      <div class="mt-6">
+        <div
+          v-if="isStoreDataLoading === true"
+          class="flex justify-start items-center gap-2"
+        >
+          <font-awesome-icon
+            icon="fa-solid fa-circle-notch"
+            class="animate-spin"
+          />
+          <p>Dateien werden geladen</p>
+        </div>
+        <div v-else-if="isStoreDataLoading === false">
+          <p>Dateien werden hier angezeigt.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { getGalleries, Gallery } from "@/composables/gallery";
+import { ref, onMounted, watch } from "vue";
 
-const data = ref();
+const galleryData = ref<Gallery[]>();
+const selectedGallery = ref<number>();
+const isStoreDataLoading = ref<boolean>();
+
+onMounted(() => {
+  getGalleries().then((d) => {
+    galleryData.value = d!;
+  });
+});
+
+let timeout: ReturnType<typeof setTimeout> = setTimeout(() => {
+  return;
+});
+
+watch(selectedGallery, () => {
+  clearTimeout(timeout);
+  isStoreDataLoading.value = true;
+  timeout = setTimeout(() => {
+    isStoreDataLoading.value = false;
+  }, 1000);
+});
 </script>
+
+<style lang="scss" scoped>
+#selected {
+  background-color: var(--red);
+  color: white;
+}
+</style>
