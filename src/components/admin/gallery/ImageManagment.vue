bb64aba18d736180df7449381b8902bf3c03d593
Nevah5 <neevaah5@gmail.com>
2022-10-27 08:50:09 +0200

    #101 Added image managment component

---
+++ src/components/admin/gallery/ImageManagment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-auto-animate class="mt-16">
+    <h2 class="text-2xl font-bold border-b-2 border-black border-b-solid mb-2">
+      Verwaltung Bilder
+    </h2>
+    <p class="mb-4">
+      Dieser Abschnitt ist für das Verwalten der Bilder und Beschreibungen
+      dieser.
+    </p>
+    <div v-if="!data" class="flex justify-start items-center">
+      <font-awesome-icon
+        icon="fa-solid fa-circle-notch"
+        class="animate-spin mr-1"
+      />
+      <p>Daten werden geladen</p>
+    </div>
+    <div v-else>Bilder werden hier Verwaltet...</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const data = ref();
+</script>
--- src/views/admin/GalleryAdministration.vue
+++ src/views/admin/GalleryAdministration.vue
@@ -1,7 +1,9 @@
 <template>
   <GalleryManagment />
+  <ImageManagment />
 </template>
 
 <script setup lang="ts">
 import GalleryManagment from "@/components/admin/gallery/GalleryManagment.vue";
+import ImageManagment from "@/components/admin/gallery/ImageManagment.vue";
 </script>
