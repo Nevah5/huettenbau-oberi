1121ef66ac272f5e6c7db252218bec62ff179554
Nevah5 <neevaah5@gmail.com>
2022-10-30 22:44:04 +0100

    #101 Optimized upload

--- src/components/admin/gallery/ImageManagment.vue
+++ src/components/admin/gallery/ImageManagment.vue
@@ -29,6 +29,7 @@
         </p>
       </div>
       <div class="mt-6">
+        <!-- Loading Info -->
         <div
           v-if="isStoreDataLoading === true"
           class="flex justify-start items-center gap-2"
@@ -39,16 +40,14 @@
           />
           <p>Dateien werden geladen</p>
         </div>
-        <div
-          v-else-if="isStoreDataLoading === false && storageData?.length === 0"
-        >
-          <h4 class="text-black">Diese Galerie hat noch keine Daten.</h4>
-        </div>
         <div v-else-if="isStoreDataLoading === false">
+          <!-- Form -->
           <form
+            v-if="selectedGallery && !isStoreDataLoading"
             class="my-6 flex justify-center items-start flex-col gap-2 w-fit"
             @submit.prevent="uploadImages"
           >
+            <!-- Input -->
             <label class="block">
               <span class="sr-only">Choose profile photo</span>
               <input
@@ -58,13 +57,15 @@
                 v-on:change="fileChange"
               />
             </label>
-            <div class="flex justify-start items-start gap-2 h-[60px]">
+            <div class="flex flex-col justify-center items-start gap-2">
+              <!-- Submit -->
               <button
                 type="submit"
                 class="py-2 px-4 bg-red text-white font-bold rounded-md"
               >
                 Upload
               </button>
+              <!-- States -->
               <div v-if="uploadStates">
                 <div
                   v-for="state in uploadStates"
@@ -76,33 +77,42 @@
                       class="bg-red flex justify-center items-center h-full text-white font-bold"
                       :style="'width:' + (400 / 100) * state.percentage + 'px'"
                     >
-                      {{ state.percentage }}%
+                      <p v-if="state.percentage !== 100">
+                        {{ state.percentage }}%
+                      </p>
+                      <p v-else>Fertig!</p>
                     </div>
                   </div>
                   <p>{{ state.name }}</p>
                 </div>
               </div>
-              <p v-else-if="errorMessage" class="font-bold text-red">
+              <p v-if="errorMessage" class="font-bold text-red">
                 {{ errorMessage }}
               </p>
             </div>
           </form>
-          <div class="flex justify-start gap-2">
-            <p class="w-[100px]">Nr.</p>
-            <p class="w-[600px]">Dateiname</p>
-          </div>
-          <div
-            v-for="(data) in (storageData as GalleryImage[])"
-            :key="data.fullPath"
-            class="flex justify-start items-center gap-2 w-fit"
-          >
-            <p class="w-[100px]">ID</p>
-            <a
-              :href="data.url"
-              class="underline text-red w-fit"
-              target="_blank"
-              >{{ data.name }}</a
+          <!-- File List -->
+          <div>
+            <div
+              class="flex justify-start gap-2"
+              v-if="storageData?.length !== 0"
             >
+              <p class="w-[100px]">Nr.</p>
+              <p class="w-[600px]">Dateiname</p>
+            </div>
+            <div
+              v-for="(data) in (storageData as GalleryImage[])"
+              :key="data.fullPath"
+              class="flex justify-start items-center gap-2 w-fit"
+            >
+              <p class="w-[100px]">ID</p>
+              <a
+                :href="data.url"
+                class="underline text-red w-fit"
+                target="_blank"
+                >{{ data.name }}</a
+              >
+            </div>
           </div>
         </div>
       </div>
@@ -128,7 +138,7 @@ const isStoreDataLoading = ref<boolean>();
 const errorMessage = ref<string>();
 const uploadStates = ref<{ name: string; percentage: number }[]>([]);
 const files = ref<FileList>();
-const validUploadTypes = ["image/jpg", "image/png"];
+const validUploadTypes = ["image/jpg", "image/png", "image/jpeg"];
 
 onMounted(() => {
   getGalleries().then((d) => {
@@ -157,6 +167,9 @@ watch(selectedGallery, () => {
 
 const selectGallery = (index: number) => {
   if (isStoreDataLoading.value === true) return;
+  uploadStates.value = [];
+  files.value = undefined;
+  errorMessage.value = "";
   selectedGallery.value = index;
 };
 const uploadImages = (): void => {
@@ -172,7 +185,7 @@ const uploadImages = (): void => {
       return;
       // eslint-disable-next-line
     } else if (!validUploadTypes.includes(files.value.item(i)?.type!)) {
-      errorMessage.value = "Es sind nur Bilder erlaubt! (.jpg, .png)";
+      errorMessage.value = "Es sind nur Bilder erlaubt! (.jpg, .png, .jpeg)";
       return;
     }
   }
@@ -187,12 +200,13 @@ const uploadImages = (): void => {
       const list = taskList! as UploadTask[];
       for (let i = 0; i < list.length; i++) {
         uploadStates.value.push({
-          name: files.value?.item(i)?.name,
+          name: files.value?.item(i)?.name || "filename.xyz",
           percentage: 0,
         });
         list[i].on("state_changed", (snapshot) => {
-          const percentage =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          const percentage = Math.floor(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
           uploadStates.value[i].percentage = percentage;
         });
       }
