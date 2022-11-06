fc23fc90f1116833efe9f4de61bace0cf8e8228c
Nevah5 <neevaah5@gmail.com>
2022-11-06 18:07:51 +0100

    Eslint

--- src/composables/config.ts
+++ src/composables/config.ts
@@ -1,36 +1,32 @@
-import { firestore } from "@/firebase"
-import { getDoc, doc, setDoc } from "firebase/firestore"
+import { firestore } from "@/firebase";
+import { getDoc, doc, setDoc } from "firebase/firestore";
 
 interface adminConfig {
-  users?: string[]
+  users?: string[];
 }
 
 const getAdminUsers = async (): Promise<adminConfig | void> => {
   return new Promise<adminConfig | void>((resolve, reject) => {
     getDoc(doc(firestore, "configs", "admins"))
-    .then((data) => {
-      resolve(data.data() as adminConfig)
-    })
-    .catch((e) => {
-      console.log(e);
-      reject()
-    })
-  })
-}
+      .then((data) => {
+        resolve(data.data() as adminConfig);
+      })
+      .catch((e) => {
+        console.log(e);
+        reject();
+      });
+  });
+};
 
 const updateAdminUsers = async (newAdminUsers: adminConfig): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
     setDoc(doc(firestore, "configs", "admins"), newAdminUsers)
-    .then(() => resolve())
-    .catch((e) => {
-      console.log(e);
-      reject()
-    })
-  })
-}
+      .then(() => resolve())
+      .catch((e) => {
+        console.log(e);
+        reject();
+      });
+  });
+};
 
-export {
-  adminConfig,
-  getAdminUsers,
-  updateAdminUsers
-}
+export { adminConfig, getAdminUsers, updateAdminUsers };
--- src/composables/navbar.ts
+++ src/composables/navbar.ts
@@ -1,52 +1,59 @@
-import { firestore } from "@/firebase"
-import { collection, doc, getDocs, orderBy, query, setDoc } from "firebase/firestore"
+import { firestore } from "@/firebase";
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+} from "firebase/firestore";
 
-interface navbarSub{
-  display: string,
-  link: string,
-  order: number
+interface navbarSub {
+  display: string;
+  link: string;
+  order: number;
 }
 
 interface navbarItem {
-  id: string,
-  display: string,
-  link?: string,
-  order: number,
-  sub?: navbarSub[]
+  id: string;
+  display: string;
+  link?: string;
+  order: number;
+  sub?: navbarSub[];
 }
 
 const getNavbarData = async () => {
-  const res = await getDocs(query(collection(firestore, "navbar"), orderBy("order", "asc")))
-  const data = res.docs.map(doc => {
-    return { id: doc.id, ...doc.data() } as navbarItem
-  })
-  data.forEach(d => {
+  const res = await getDocs(
+    query(collection(firestore, "navbar"), orderBy("order", "asc"))
+  );
+  const data = res.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() } as navbarItem;
+  });
+  data.forEach((d) => {
     d.sub?.sort((a, b) => {
-      return a.order - b.order
-    })
-  })
-  return (data as navbarItem[])
-}
+      return a.order - b.order;
+    });
+  });
+  return data as navbarItem[];
+};
 
-const updateNavbarDocument = (docId: string, newValue: navbarItem): Promise<void> => {
+const updateNavbarDocument = (
+  docId: string,
+  newValue: navbarItem
+): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
     setDoc(doc(firestore, "navbar", docId), {
       display: newValue.display,
       link: newValue.link,
       order: newValue.order,
-      sub: newValue.sub
+      sub: newValue.sub,
     })
-    .then(() => resolve())
-    .catch((e) => {
-      console.log(e);
-      reject()
-    })
-  })
-}
+      .then(() => resolve())
+      .catch((e) => {
+        console.log(e);
+        reject();
+      });
+  });
+};
 
-export {
-  navbarSub,
-  navbarItem,
-  getNavbarData,
-  updateNavbarDocument
-}
+export { navbarSub, navbarItem, getNavbarData, updateNavbarDocument };
