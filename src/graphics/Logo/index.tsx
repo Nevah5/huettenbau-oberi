9a02fb853cf22881715bb6bc99ff3fa478de6bb8
Nevah5 <contact@nevah5.dev>
2026-01-09 22:48:39 +0100

    Optimized mapped logo component

--- src/graphics/Logo/index.tsx
+++ src/graphics/Logo/index.tsx
@@ -5,23 +5,31 @@ import { getPayload } from 'payload'
 import config from '@payload-config'
 
 export const Logos = async () => {
-  const payload = await getPayload({ config })
+  const payload = await getPayload({ config: config })
   const settings = await payload.findGlobal({ slug: 'settings' })
 
   const lightModeLogo = settings.lightModeLogo as Media
   const darkModeLogo = settings.darkModeLogo as Media
 
+  if (!lightModeLogo?.url || !darkModeLogo?.url) {
+    return (
+      <div>
+        <p>Logo not found. Please upload logos in the settings.</p>
+      </div>
+    )
+  }
+
   return (
     <>
       <Image
-        src={lightModeLogo.url || ''}
+        src={lightModeLogo.url}
         alt={lightModeLogo.alt || 'Logo'}
         width={lightModeLogo.width || 640}
         height={lightModeLogo.height || 360}
         className={'light-mode-image'}
       />
       <Image
-        src={darkModeLogo.url || ''}
+        src={darkModeLogo.url}
         alt={darkModeLogo.alt || 'Logo'}
         width={darkModeLogo.width || 640}
         height={darkModeLogo.height || 360}
